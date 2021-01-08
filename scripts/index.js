e435cf8ac67bf3872299ad3462e8134ff7e40a8e
unknown <30lmas09@gmail.com>
2021-01-08 17:00:58 +0530

    new edit

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,41 @@
-Name = document.getElementById("name")
-Name.value="Arav"
+
+document.addEventListener("scroll", () => {
+    console.log(window.scrollY ) // for web
+    //console.log(window.scrollY>1850) // for mobile
+    var isMobile = window.orientation > -1; 
+    
+    if(isMobile){
+        if((window.scrollY >= 1850)){
+            document.getElementById("navbar").style.backgroundColor = "white"
+            document.getElementById("name").style.color = "black"
+            document.getElementById("links1").style.color = "black"
+            document.getElementById("links2").style.color = "black"
+            document.getElementById("links3").style.color = "black"  
+        }
+        else{
+            document.getElementById("navbar").style.backgroundColor = "black"
+            document.getElementById("name").style.color = "white"
+            document.getElementById("links1").style.color = "white"
+            document.getElementById("links2").style.color = "white"
+            document.getElementById("links3").style.color = "white"  
+        }
+    }
+    else{
+        if((window.scrollY > 1200)){
+            document.getElementById("navbar").style.backgroundColor = "white"
+            document.getElementById("name").style.color = "black"
+            document.getElementById("links1").style.color = "black"
+            document.getElementById("links2").style.color = "black"
+            document.getElementById("links3").style.color = "black"  
+        }
+        else{
+            document.getElementById("navbar").style.backgroundColor = "black"
+            document.getElementById("name").style.color = "white"
+            document.getElementById("links1").style.color = "white"
+            document.getElementById("links2").style.color = "white"
+            document.getElementById("links3").style.color = "white"  
+        }
+    }
+})
+
+
