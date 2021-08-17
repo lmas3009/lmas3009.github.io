ff8219662ed1b367f175f0ba0b3665e401b31fc4
Aravind Kumar Vemula <60876387+lmas3009@users.noreply.github.com>
2021-08-17 23:01:46 +0530

    new changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,38 @@
+// document.addEventListener("scroll", () => {
+//     console.log(window.scrollY) // for web
+//     //console.log(window.scrollY>1850) // for mobile
+//     var isMobile = window.orientation > -1;
+//     if(isMobile){
+//         if((window.scrollY >= 1850 && window.scrollY<=3310)){
+//             document.getElementById("navbar").style.backgroundColor = "white"
+//             document.getElementById("name").style.color = "black"
+//             document.getElementById("links1").style.color = "black"
+//             document.getElementById("links2").style.color = "black"
+//             document.getElementById("links3").style.color = "black"
+//         }
 
-document.addEventListener("scroll", () => {
-    console.log(window.scrollY) // for web
-    //console.log(window.scrollY>1850) // for mobile
-    var isMobile = window.orientation > -1; 
-    if(isMobile){
-        if((window.scrollY >= 1850 && window.scrollY<=3310)){
-            document.getElementById("navbar").style.backgroundColor = "white"
-            document.getElementById("name").style.color = "black"
-            document.getElementById("links1").style.color = "black"
-            document.getElementById("links2").style.color = "black"
-            document.getElementById("links3").style.color = "black"  
-        }
-         
-        else{
-            document.getElementById("navbar").style.backgroundColor = "black"
-            document.getElementById("name").style.color = "white"
-            document.getElementById("links1").style.color = "white"
-            document.getElementById("links2").style.color = "white"
-            document.getElementById("links3").style.color = "white"  
-        }
-    }
-    else{
-        if((window.scrollY > 1200 && window.scrollY < 1800)){
-            document.getElementById("navbar").style.backgroundColor = "white"
-            document.getElementById("name").style.color = "black"
-            document.getElementById("links1").style.color = "black"
-            document.getElementById("links2").style.color = "black"
-            document.getElementById("links3").style.color = "black"  
-        }
-        else{
-            document.getElementById("navbar").style.backgroundColor = "black"
-            document.getElementById("name").style.color = "white"
-            document.getElementById("links1").style.color = "white"
-            document.getElementById("links2").style.color = "white"
-            document.getElementById("links3").style.color = "white"  
-        }
-    }
-})
-
-
+//         else{
+//             document.getElementById("navbar").style.backgroundColor = "black"
+//             document.getElementById("name").style.color = "white"
+//             document.getElementById("links1").style.color = "white"
+//             document.getElementById("links2").style.color = "white"
+//             document.getElementById("links3").style.color = "white"
+//         }
+//     }
+//     else{
+//         if((window.scrollY > 1200 && window.scrollY < 1800)){
+//             document.getElementById("navbar").style.backgroundColor = "white"
+//             document.getElementById("name").style.color = "black"
+//             document.getElementById("links1").style.color = "black"
+//             document.getElementById("links2").style.color = "black"
+//             document.getElementById("links3").style.color = "black"
+//         }
+//         else{
+//             document.getElementById("navbar").style.backgroundColor = "black"
+//             document.getElementById("name").style.color = "white"
+//             document.getElementById("links1").style.color = "white"
+//             document.getElementById("links2").style.color = "white"
+//             document.getElementById("links3").style.color = "white"
+//         }
+//     }
+// })
