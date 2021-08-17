a968e8b5e87c33b380cc423ab9aa6184d1111303
Aravind Kumar Vemula <60876387+lmas3009@users.noreply.github.com>
2021-08-17 22:40:26 +0530

    new changes

---
+++ scripts/landing.js
@@ -0,0 +1,17 @@
+const logo = document.querySelectorAll("#namelogo1 path");
+
+for (let i = 0; i < logo.length; i++) {
+  console.log(logo[i].getTotalLength());
+}
+
+setTimeout(function () {
+  document.querySelector(".navbar").style.display = "block";
+  document.querySelector(".container").style.display = "block";
+  document.querySelector(".skilltoolcontainer").style.display = "block";
+  document.querySelector(".projects").style.display = "block";
+  document.querySelector(".navbottom").style.display = "block";
+  document.querySelector(".footer").style.display = "block";
+  document.querySelector(".intro").style.display = "block";
+  document.querySelector(".blogs").style.display = "block";
+  document.querySelector("#namelogo").style.display = "none";
+}, 8000);
